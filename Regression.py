74489b988bf17181a0416205662ff2cb808c9338
selkis4u <selkis@nate.com>
2026-09-16 08:54:22 +0900

    add

--- Regression.py
+++ Regression.py
@@ -36,7 +36,7 @@ print(f"설명력: {base_r2:.4f}")
 
 #기준모델과 비교
 error_reduction = ((base_mae - mae) / base_mae) * 100
-print(f"기존 모델 평균 오차 개선률: {error_reduction:.1f}%")
+print(f"기존 모델 평균 오차 개선률: {error_reduction:.1f}")
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.dummy import DummyClassifier
